e239c39c911b0baa368b03398e357cbc76312452
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2022-04-14 20:13:10 -0300

    centralizing exports

---
+++ lib/index.ts
@@ -0,0 +1,25 @@
+import createTable from "./createTable/createTable"
+import getConsistency from "./getConsistency/getConsistency"
+import getConsistencyIndex from "./getConsistencyIndex/getConsistencyIndex"
+import getConsistencyRatio from "./getConsistencyRatio/getConsistencyRatio"
+import getFinalPriorities from "./getFinalPriorities/getFinalPriorities"
+import getLambdaMax from "./getLambdaMax/getLambdaMax"
+import getSaatyScaleScore from "./getSaatyScaleScore/getSaatyScaleScore"
+import ponderation from "./ponderation/ponderation"
+import priorityByItem from "./priorityByItem/priorityByItem"
+import serializeData from "./serializeData/serializeData"
+import sumColumns from "./sumColumns/sumColumns"
+
+export {
+  createTable,
+  getConsistency,
+  getConsistencyIndex,
+  getConsistencyRatio,
+  getFinalPriorities,
+  getLambdaMax,
+  getSaatyScaleScore,
+  ponderation,
+  priorityByItem,
+  serializeData,
+  sumColumns,
+};
