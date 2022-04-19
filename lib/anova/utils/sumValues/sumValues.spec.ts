9dc5ab3ba6fc626f783a03deaeccb0900a695d0a
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2022-04-19 15:59:05 -0300

    reafeat: sum + calcs

--- lib/anova/utils/sumValues/sumValues.spec.ts
+++ lib/anova/utils/sumValues/sumValues.spec.ts
@@ -4,7 +4,12 @@ describe("Sum values function", () => {
   it("Should return the sum of the values of an array.", () => {
     const result = sumValues([1, 2, null, 4, 5, null, 7, 8]);
 
-    expect(result).toBe(27);
+    expect(result).toHaveProperty('sum');
+    expect(result).toHaveProperty('calc');
+    expect(result.sum).toBe(27);
+    expect(result.calc).toBe(
+      "$1+2+4+5+7+8=27$"
+    );
   });
 
   it("Should return the sum of the values of a multidimensional array.", () => {
@@ -13,9 +18,14 @@ describe("Sum values function", () => {
         [1, 2, null],
         [4, null, 6],
         [[7], 8, 9]
-      ]
+      ],
     );
-
-    expect(result).toBe(37);
+    
+    expect(result).toHaveProperty('sum');
+    expect(result).toHaveProperty('calc');
+    expect(result.sum).toBe(37);
+    expect(result.calc).toBe(
+      "$1+2+4+6+7+8+9=37$"
+    );
   });
 });
--- lib/anova/utils/sumValues/sumValues.ts
+++ lib/anova/utils/sumValues/sumValues.ts
@@ -1,24 +1,45 @@
+interface SumValuesReturn {
+  sum: number;
+  calc: string;
+}
+
+/**
+ * Object containing the return of sumValues function
+ * @typedef {Object} SumValuesReturn
+ * @property {number} sum - The sum result
+ * @property {string} calc - The calculations made to get the sum
+ */
+
 /**
  * Sum the values of the array
  * @param {any[]} arr - Uni or multidimensional array
  * @returns {number} The sum result
  */
-function sumValues(arr: Array<any>): number {
-  let total = 0;
+function sumValues(arr: Array<any>): SumValuesReturn {
+  let sum = 0;
+  let calc = '$';
 
-  for (let i = 0; i < arr.length; i++) {
+  for (let i = 0; i < arr.length; i++) {    
     if (Array.isArray(arr[i])) {
-      total += sumValues(arr[i]);
+      let { sum: currSum, calc: currCalc } = sumValues(arr[i])      
+      
+      currCalc = currCalc.replace(/\$|=\d+/g, '');
+      
+      sum += currSum;
+      calc += `${calc === '$' ? '' : '+'}${currCalc}`;
+ 
       continue;
     }
 
     // sum only if it is a number
-    if(!Number.isNaN(arr[i])) {
-      total += arr[i];
+    if(arr[i] && !Number.isNaN(arr[i])) {
+      sum += arr[i];      
+      calc += `${calc === '$' ? '' : '+'}${arr[i]}`;
     }    
   }
-
-  return total;
+  calc += `=${sum}$`;
+  
+  return { sum, calc, };
 }
 
 export default sumValues;
