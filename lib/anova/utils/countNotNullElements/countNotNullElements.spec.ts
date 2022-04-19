a19af11d4f77de02bfbaf4dda9e1e9960d793374
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2022-04-19 14:06:27 -0300

    fix typo

--- lib/anova/utils/countNotNullElements/countNotNullElements.spec.ts
+++ lib/anova/utils/countNotNullElements/countNotNullElements.spec.ts
@@ -1,13 +1,13 @@
 import countNotNullElements from "./countNotNullElements";
 
 describe("Count not null elements function", () => {
-  it("Should return de number of not null elements of an array", () => {
+  it("Should return the number of not null elements of an array", () => {
     const result = countNotNullElements([1, 2, null, 4, 5, null, 7, 8]);
 
     expect(result).toBe(6);
   });
 
-  it("Should return de number of not null elements of a multidimensional array", () => {
+  it("Should return the number of not null elements of a multidimensional array", () => {
     const result = countNotNullElements(
       [
         [1, 2, null],
