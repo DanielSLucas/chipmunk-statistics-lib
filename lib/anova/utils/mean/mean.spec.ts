578298c54247e3bbf9141219d63a6774fe12f968
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2022-04-19 16:19:19 -0300

    refeat: mean with calcs

--- lib/anova/utils/mean/mean.spec.ts
+++ lib/anova/utils/mean/mean.spec.ts
@@ -4,7 +4,12 @@ describe("Mean function", () => {
   it("Should return the mean of an array of values", () => {
     const result = mean([1, 2, 3, 5]);
 
-    expect(result).toBe(2.75);
+    expect(result).toHaveProperty("mean");
+    expect(result).toHaveProperty("calc");
+    expect(result.mean).toBe(2.75);
+    expect(result.calc).toBe(
+      "$frac{1+2+3+5}{4}$"
+    );
   });
 
   it("Should return the mean of a multidimensional array of values", () => {
@@ -15,6 +20,11 @@ describe("Mean function", () => {
       ],
     );
 
-    expect(result).toBe(3.5);
+    expect(result).toHaveProperty("mean");
+    expect(result).toHaveProperty("calc");
+    expect(result.mean).toBe(3.5);
+    expect(result.calc).toBe(
+      "$frac{1+2+3+4+5+6}{6}$"
+    );
   });
 });
--- lib/anova/utils/mean/mean.ts
+++ lib/anova/utils/mean/mean.ts
@@ -1,16 +1,33 @@
 import { countNotNullElements, sumValues } from "..";
 
+interface MeanReturn {
+  mean: number;
+  calc: string;
+}
+
+/**
+ * Object containing the return of mean function
+ * @typedef {Object} MeanReturn
+ * @property {number} mean - The mean result
+ * @property {string} calc - The calculations made to get the mean
+ */
+
 /**
  * Returns the mean for a given array of values
  * @param {any[]} values Array of numeric values
  * @returns {number} The mean
  */
-function mean(values: Array<any>): number {
+function mean(values: Array<any>): MeanReturn {
   const numberOfValues = countNotNullElements(values);
 
-  const sumOfValues = sumValues(values);
+  let { sum, calc: sumCalc } = sumValues(values);
 
-  return sumOfValues/numberOfValues;
+  sumCalc = sumCalc.replace(/\$|=\d+/g, '');
+
+  const calc = `$frac{${sumCalc}}{${numberOfValues}}$`;
+  const mean = sum/numberOfValues;
+
+  return { mean, calc };
 }
 
 export default mean;
